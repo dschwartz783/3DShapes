115113aa3f3da50f105651e840d8ad8309625d2b
dschwartz783 <dschwartz783@gmail.com>
2017-01-22 23:45:06 -0500

    works with swift3 now

--- 3DShapes/AppDelegate.swift
+++ 3DShapes/AppDelegate.swift
@@ -15,21 +15,19 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var pressLabel: NSTextField!
     @IBOutlet weak var numPointsField: NSTextField!
-    let calcQueue: dispatch_queue_t = dispatch_queue_create("calcQueue", DISPATCH_QUEUE_CONCURRENT)
-    let arrayQueue: dispatch_queue_t = dispatch_queue_create("arrayQueue", DISPATCH_QUEUE_SERIAL)
+    let calcQueue = DispatchQueue(label: "calcQueue", qos: DispatchQoS.userInteractive, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.inherit, target: nil)
+    let arrayQueue = DispatchQueue(label: "arrayQueue")
     var coordinateArray: Array<SCNVector3> = []
     var indices: [Int] = []
     @IBOutlet weak var mainScene: SCNView!
     
-    func drawArt(window: NSView)
-    {
+    func drawArt(window: NSView) {
         let begin_time = NSDate().timeIntervalSince1970
-        let numPoints = CGFloat((self.numPointsField.stringValue as NSString).doubleValue)
-        if numPoints > 0
-        {
+        let numPoints = (self.numPointsField.stringValue as NSString).doubleValue
+        if numPoints > 0 {
             let mainPath: NSBezierPath = NSBezierPath()
             mainPath.lineWidth = 0.25
-            NSColor.greenColor().set()
+            NSColor.green.set()
             var smallestDimension: CGFloat
             if (window.frame.height < window.frame.width)
             {
@@ -44,18 +42,18 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
             
             self.coordinateArray.removeAll()
             
-            for (var theta: CGFloat = 0; theta <= CGFloat(M_PI); theta += CGFloat(M_PI) / CGFloat(numPoints) * 2)
-            {
-                    dispatch_apply(Int(numPoints), calcQueue, { (n: Int) -> Void in
-                        var phi = M_PI * Double(2) / Double(numPoints) * Double(n)
-                        let newVector = SCNVector3Make(r * CGFloat(sin(theta)) * CGFloat(cos(phi)), r * CGFloat(sin(theta)) * CGFloat(sin(phi)), r * CGFloat(cos(theta)))
-                            dispatch_sync(self.arrayQueue, { () -> Void in
-                                self.coordinateArray.append(newVector)
-                            })
+            for theta: Double in stride(from: 0 as Double, to: M_PI, by: Double(M_PI) / numPoints * 2) {
+                DispatchQueue.concurrentPerform(iterations: Int(numPoints), execute: { (n: Int) in
+                    let phi = M_PI * Double(2) / Double(numPoints) * Double(n)
+                    let newVector = SCNVector3Make(r * CGFloat(sin(theta)) * CGFloat(cos(phi)), r * CGFloat(sin(theta)) * CGFloat(sin(phi)), r * CGFloat(cos(theta)))
+                    self.arrayQueue.sync(execute: { () -> Void in
+                        self.coordinateArray.append(newVector)
                     })
+                })
                 
             }
-            dispatch_barrier_async(calcQueue, { () -> Void in
+            
+            calcQueue.async(qos: DispatchQoS.userInteractive, flags: DispatchWorkItemFlags.barrier, execute: {
                 
                 let testScene = SCNScene()
                 
@@ -69,12 +67,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
                         let junkNode2: SCNNode = SCNNode()
                         junkNode2.position = coord2
                         let newNode = self.lineBetweenNodeA(junkNode1, nodeB: junkNode2)
-
+                        
                         testScene.rootNode.addChildNode(newNode)
                         
                     }
                 }
-                dispatch_sync(dispatch_get_main_queue(), { () -> Void in
+                DispatchQueue.main.sync(execute: { () -> Void in
                     self.mainScene.autoenablesDefaultLighting = true
                     self.mainScene.allowsCameraControl = true
                     self.mainScene.scene = testScene
@@ -84,26 +82,26 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
         let totalTime = NSDate().timeIntervalSince1970 - begin_time
     }
     
-    func lineBetweenNodeA(nodeA: SCNNode, nodeB: SCNNode) -> SCNNode
+    func lineBetweenNodeA(_ nodeA: SCNNode, nodeB: SCNNode) -> SCNNode
     {
         let positions: [CGFloat] = [nodeA.position.x, nodeA.position.y, nodeA.position.z, nodeB.position.x, nodeB.position.y, nodeB.position.z]
-        let positionData = NSData(bytes: positions, length: sizeof(CGFloat)*positions.count)
+        let positionData = NSData(bytes: positions, length: MemoryLayout<CGFloat>.size*positions.count)
         let indices: [Int32] = [0, 1]
-        let indexData = NSData(bytes: indices, length: sizeof(Int32) * indices.count)
+        let indexData = NSData(bytes: indices, length: MemoryLayout<Int32>.size * indices.count)
         
-        let source = SCNGeometrySource(data: positionData, semantic: SCNGeometrySourceSemanticVertex, vectorCount: indices.count, floatComponents: true, componentsPerVector: 3, bytesPerComponent: sizeof(CGFloat), dataOffset: 0, dataStride: sizeof(CGFloat) * 3)
-        let element = SCNGeometryElement(data: indexData, primitiveType: SCNGeometryPrimitiveType.Line, primitiveCount: indices.count, bytesPerIndex: sizeof(Int32))
+        let source = SCNGeometrySource(data: positionData as Data, semantic: SCNGeometrySource.Semantic.vertex, vectorCount: indices.count, usesFloatComponents: true, componentsPerVector: 3, bytesPerComponent: MemoryLayout<CGFloat>.size, dataOffset: 0, dataStride: MemoryLayout<CGFloat>.size * 3)
+        let element = SCNGeometryElement(data: indexData as Data, primitiveType: SCNGeometryPrimitiveType.line, primitiveCount: indices.count, bytesPerIndex: MemoryLayout<Int32>.size)
         
         let line = SCNGeometry(sources: [source], elements: [element])
         return SCNNode(geometry: line)
     }
-
+    
     @IBAction func drawPushed(sender: NSButton)
     {
         //self.mainScene.backgroundColor = NSColor.blackColor()
-        sender.enabled = false
-        self.drawArt(self.mainScene)
-        sender.enabled = true
+        sender.isEnabled = false
+        self.drawArt(window: self.mainScene)
+        sender.isEnabled = true
     }
     
     @IBAction func buttonPressed(sender: NSButton)
@@ -117,7 +115,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
         // Insert code here to initialize your application
     }
     
-    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool
     {
         return true;
     }
@@ -128,8 +126,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, SCNSceneRe
         if window.frame.width < window.frame.height {
             cur_size = window.frame.width
         }
-        var compensated_x = sin(angle / CGFloat(180.0) * CGFloat(M_PI)) * (cur_size - 60) / 2 + window.frame.width / 2
-        var compensated_y = cos(angle / CGFloat(180.0) * CGFloat(M_PI)) * (cur_size - 60) / 2 + window.frame.height / 2
+        let compensated_x = sin(angle / CGFloat(180.0) * CGFloat(M_PI)) * (cur_size - 60) / 2 + window.frame.width / 2
+        let compensated_y = cos(angle / CGFloat(180.0) * CGFloat(M_PI)) * (cur_size - 60) / 2 + window.frame.height / 2
         return (compensated_x, compensated_y)
     }
 }
